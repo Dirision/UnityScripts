039c643d78300b9bb38b3ef49b9962e66c28e43b
Nicholas DiRisio <nick.dirisio@mail.utoronto.ca>
2018-07-04 01:33:01 -0400

    added some field variables for sensitivity
    Also began work on freelook when holding shift (button does not matter, can change later)

--- 3rdPersonCameraController/CameraController.cs
+++ 3rdPersonCameraController/CameraController.cs
@@ -7,16 +7,20 @@ public class CameraController : MonoBehaviour {
     public Transform cameraTransform;
     public Transform targetTransform;
 
+
+    public float xSensitivity =5f;
+    public float ySensitivity =5f;
+    public float orbitDampening = 10f;
     private Vector3 offset;
 
 
     /* TODO:
 
-        ADD proper variables for mouse sensitivity (instead of just "5") 
         ADD toggle so that mouse movement does not move player actor 
+        L-> FIX press shift to freelook
+
         ADD better smothening to camera (by editing the Lerp call or outright removal of Lerp)
-
-
+        
     */
 
 	// Use this for initialization
@@ -36,23 +40,32 @@ public class CameraController : MonoBehaviour {
 
 	}
     
-   
     private void LateUpdate()
     {
+        Quaternion yRotation =
+                Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * ySensitivity, -Vector3.right);
 
-        Quaternion xRot =
-               Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * 5, Vector3.right);
+        offset = yRotation * offset;
 
-        offset = xRot * offset;
+        Quaternion xRotation;
+        // Rotate while turning player
+        if (! Input.GetKey(KeyCode.LeftShift))
+        {
+            float horizontal = Input.GetAxis("Mouse X") * xSensitivity;
+            targetTransform.Rotate(0, horizontal, 0);
+            float desiredYAngle = targetTransform.eulerAngles.y;
+            xRotation = Quaternion.Euler(0, desiredYAngle, 0);
+            transform.position = Vector3.Lerp(targetTransform.position, targetTransform.position - xRotation * offset, orbitDampening);
 
-        float horizontal = Input.GetAxis("Mouse X") * 5;
-        targetTransform.Rotate(0, horizontal, 0);
+        }
+        else
+        {
+            xRotation = Quaternion.Euler(0,Input.GetAxis("Mouse X") * xSensitivity,0);
 
-        float desiredYAngle = targetTransform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
-
+            transform.position = Vector3.Lerp(targetTransform.position,  targetTransform.position - xRotation* offset, orbitDampening);
+        }
         // transform.position = targetTransform.position - (rotation * offset);
-        transform.position = Vector3.Lerp(targetTransform.position, targetTransform.position - rotation * offset, 10);
+        
         transform.LookAt(targetTransform);
     }
 
